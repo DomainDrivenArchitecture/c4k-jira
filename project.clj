04e372de91112d770c39d3674ef3ca1837a6e9d7
bom <mattis.boeckle@meissa-gmbh.de>
2022-06-10 10:09:26 +0200

    Version 1.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-jira "1.1.3-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-jira "1.1.3"
   :description "jira c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
