e6488d1f88dbab4cfd48202870789648ba198d20
Clemens Geibel <clemens.geibel@meissa-gmbh.de>
2022-07-15 15:02:01 +0200

    Use new backup removal policy

--- infrastructure/docker-backup/image/resources/backup.sh
+++ infrastructure/docker-backup/image/resources/backup.sh
@@ -5,7 +5,8 @@ set -o pipefail
 function main() {
     file_env AWS_ACCESS_KEY_ID
     file_env AWS_SECRET_ACCESS_KEY
-    file_env RESTIC_DAYS_TO_KEEP 14
+    file_env RESTIC_DAYS_TO_KEEP 30
+    file_env RESTIC_MONTHS_TO_KEEP 12
 
     backup-fs-from-directory '/var/backups/' 'export/'
 }
